4bd2ceb9ccd2f19c2da6c02e7fb5fce5eddbdd94
Evan Fredericks <evan.fredericks@gmail.com>
2022-04-07 09:29:29 -0500

    delete customer

--- src/test/java/guru/springfamework/api/v1/services/CustomerServiceTest.java
+++ src/test/java/guru/springfamework/api/v1/services/CustomerServiceTest.java
@@ -3,6 +3,8 @@ package guru.springfamework.api.v1.services;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -120,5 +122,15 @@ public class CustomerServiceTest {
         //then
         assertEquals(customerDTO.getFirstname(), savedDto.getFirstname());
         assertEquals("/api/v1/customer/1", savedDto.getUrl());
+    }
+
+    @Test
+    public void deleteCustomerById() throws Exception {
+
+        Long id = 1L;
+
+        customerService.deleteCustomerById(id);
+
+        verify(customerRepository, times(1)).deleteById(anyLong());
     }
 }
